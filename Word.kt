269490a6e2618379d79147f12c450053abdbfd28
BC <admin@superzen.org>
2023-02-28 21:07:36 -0700

    Update Word.kt

--- Word.kt
+++ Word.kt
@@ -17,3 +17,4 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
+// todo: run with ktor
